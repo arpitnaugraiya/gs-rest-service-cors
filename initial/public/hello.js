6b1162a55d9bf08b1fda4b0ec71ec5ff483113f6
arpitnaugraiya <sonu.be07@gmail.com>
2017-01-22 02:02:49 +0530

    rest-service-cors done!

---
+++ initial/public/hello.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$.ajax({
+		url: "http://localhost:9000/greeting-javaconfig"	
+		}).then(function(data, status, jqxhr){
+			$('.greeting-id').append(data.id);
+			$('.greeting-content').append(data.content);
+			console.log(jqxhr);
+
+	});
+
+
+});
